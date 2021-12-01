fc013b73da0989e3304a3e5a504bd45c1bbd563d
Adam Abernathy <amabernathy@ku.edu>
2021-11-30 18:18:52 -0600

    inital format done

---
+++ js/user_form_check.js
@@ -0,0 +1,14 @@
+//formChecker.js
+// checks that for is filled in, user is an email, password is not blank
+
+
+
+function validateForm() {
+  let newUser = document.forms["new_user"]["password"].value;
+  //check password
+  if (newUser == "") {
+    alert("USER NAME must be filled out");
+    return false;
+  }
+
+}
