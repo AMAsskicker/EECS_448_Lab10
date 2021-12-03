33b48a0e94309b7a22b9dfd0c40459091d1bf7b7
Adam Abernathy <amabernathy@ku.edu>
2021-12-02 23:43:49 -0600

    interface and php sql first pass

--- php/create_user.php
+++ php/create_user.php
@@ -1,3 +1,41 @@
 <?php
-  
+  //vars
+  $can_create = true;
+  $mysqli = new mysqli("mysql.eecs.ku.edu", "a682a575", "Xaisifi7", "a682a575");
+  $get_query = "SELECT * FROM Users";
+  //get user field
+  $new_user_name = $_POST["userName"];
+  /* check connection */
+  if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+  }
+  // get query
+  if ($result = $mysqli->query($get_query)) {
+    //check for existing name in db
+    while ($row = $result->fetch_assoc()) {
+      if ($row["user_id"] == $new_user_name) {
+        $can_create = false;
+      }
+    }
+  }
+  //not in use, create new row
+  // $add_row = "INSERT INTO Users (user_id) VALUES ('".$new_user_name."')";
+  // '" . $_POST['headline'] " . ', '" . $_POST['content'] . "'
+  if ($can_create) {
+    $add_result = mysqli_query($mysqli, "INSERT INTO Users(user_id) VALUES($new_user_name)");
+    if ($add_result) {
+      echo "New record created successfully";
+    } else {
+      echo "Error: NEW USER NOT CREATED";
+    }
+  }
+
+
+
+  //free results, close connection
+  $result->free();
+  $add_result->free();
+  $mysqli->close();
+
  ?>
