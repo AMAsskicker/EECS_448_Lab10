562034b5c38abf585cb20b6a33a0d2dba75a6c9f
Adam Abernathy <amabernathy@ku.edu>
2021-12-02 21:44:43 -0600

    php updates

--- php/view_users.php
+++ php/view_users.php
@@ -1,3 +1,20 @@
 <?php
-  
+  $mysqli = new mysqli("mysql.eecs.ku.edu", "a682a575", "Xaisifi7", "a682a575");
+/* check connection */
+  if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+  }
+  $query = "SELECT user_id FROM Users";
+  if ($result = $mysqli->query($query)) {
+/* fetch associative array */
+    while ($row = $result->fetch_assoc()) {
+      printf ("%s (%s)\n", $row["User ID"]);
+      echo '<span id="user_print">'. $row .'</span>';
+    }
+/* free result set */
+    $result->free();
+  }
+/* close connection */
+  $mysqli->close();
  ?>
